af4a660ce3d077e61a2d94e16edf2391546a0a90
Mark Dickinsonm <dickinsonmr1@gmail.com>
2023-12-10 07:30:02 -0500

    Doom-style player/camera movement

--- src/blasterScene.ts
+++ src/blasterScene.ts
@@ -9,6 +9,11 @@ export default class BlasterScene extends THREE.Scene {
 
     private readonly camera: THREE.PerspectiveCamera;
 
+    private readonly keyDown = new Set<string>();
+
+    private blaster?: THREE.Group;
+    private directionVector = new THREE.Vector3();
+
     constructor(camera: THREE.PerspectiveCamera) {
         super();
         
@@ -39,12 +44,12 @@ export default class BlasterScene extends THREE.Scene {
 
         this.add(t1, t2, t3, t4);
 
-        const blaster = await this.createBlaster();
-        this.add(blaster);
-        blaster.position.z = -1;
+        this.blaster = await this.createBlaster();
+        this.add(this.blaster);
+        this.blaster.position.z = -1;
 
         // attach blaster to camera
-        blaster.add(this.camera);
+        this.blaster.add(this.camera);
         this.camera.position.z = 1;
         this.camera.position.y = 0.5;
 
@@ -59,6 +64,69 @@ export default class BlasterScene extends THREE.Scene {
         const light = new THREE.DirectionalLight(0xFFFFFF, 1);
         light.position.set(0, 4, 2);
         this.add(light);
+
+        document.addEventListener('keydown', this.handleKeyDown);
+        document.addEventListener('keyup', this.handleKeyUp);
+
+    }
+
+    private handleKeyDown = (event: KeyboardEvent) => {
+        this.keyDown.add(event.key.toLowerCase());
+    }
+
+	private handleKeyUp = (event: KeyboardEvent) => {
+		this.keyDown.delete(event.key.toLowerCase())
+
+		if (event.key === ' ')
+		{
+			//this.createBullet()
+		}
+	}
+
+    private updateInput() {
+        if(!this.blaster) {
+            return;
+        }
+
+        const shiftKey = this.keyDown.has('shift');
+        if(!shiftKey){
+            if(this.keyDown.has('a') || this.keyDown.has('arrowleft')) {
+                this.blaster.rotateY(0.02);
+            }
+            else if(this.keyDown.has('d') || this.keyDown.has('arrowright')) {
+                this.blaster.rotateY(-0.02);
+            }
+        }
+
+        const dir = this.directionVector;
+
+        this.camera.getWorldDirection(dir);
+        const speed = 0.1;
+
+        if(this.keyDown.has('w') || this.keyDown.has('arrowup')) {
+            this.blaster.position.add(dir.clone().multiplyScalar(speed));
+        }
+        else if(this.keyDown.has('s') || this.keyDown.has('arrowdown')) {
+            this.blaster.position.add(dir.clone().multiplyScalar(-speed));
+        }
+
+        if(shiftKey) {
+            const strafeDir = dir.clone();
+            const upVector = new THREE.Vector3(0, 1, 0);
+
+            if(this.keyDown.has('a') || this.keyDown.has('arrowleft')) {
+                this.blaster.position.add(
+                    strafeDir.applyAxisAngle(upVector, Math.PI * 0.5)
+                        .multiplyScalar(speed)
+                );
+            }
+            else if(this.keyDown.has('d') || this.keyDown.has('arrowright')) {
+                this.blaster.position.add(
+                    strafeDir.applyAxisAngle(upVector, -Math.PI * 0.5)
+                        .multiplyScalar(speed)
+                );
+            }
+        }
     }
 
     private async createTarget(mtl: MTLLoader.MaterialCreator) {
@@ -82,5 +150,6 @@ export default class BlasterScene extends THREE.Scene {
 
     update() {
         // update
+        this.updateInput();
     }
 }
