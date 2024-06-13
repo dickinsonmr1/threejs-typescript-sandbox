e919b3c23c209c778b40d33392a46f98518b0269
Mark Dickinson <dickinsonmr1@gmail.com>
2024-06-12 21:10:23 -0400

    initial commit for TerrainConfigParameters

--- src/gameobjects/shapes/terrainObject.ts
+++ src/gameobjects/shapes/terrainObject.ts
@@ -3,6 +3,28 @@ import * as CANNON from 'cannon-es'
 import { Utility } from "../../utility";
 import { TextureToArray } from "./textureToArray";
 
+export interface TerrainConfigParameters {
+  // TODO: use in TerrainObject
+  level1Texture: string;
+  level1MinY: number;
+  level1MaxY: number;
+
+  level2Texture: string;
+  level2MinY: number;
+  level2MaxY: number;
+
+  level3Texture: string;
+  level3MinY: number;
+  level3MaxY: number;
+
+  level4Texture: string;
+  level4MinY: number;
+  level4MaxY: number;
+
+  level5Texture: string;
+  level5MinY: number;
+  level5MaxY: number;
+}
 
 // TODO: combine PlaneObject and GroundObject (in progress)
 export class TerrainObject {
